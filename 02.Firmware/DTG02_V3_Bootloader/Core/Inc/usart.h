7402267009d1b287ec3a587692888a17d29d05b2
huybk213 <huybk213@gmail.com>
2021-12-10 10:25:00 +0700

    Update G2V3 bootloader

---
+++ 02.Firmware/DTG02_V3_Bootloader/Core/Inc/usart.h
@@ -0,0 +1,97 @@
+/**
+  ******************************************************************************
+  * @file    usart.h
+  * @brief   This file contains all the function prototypes for
+  *          the usart.c file
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __USART_H__
+#define __USART_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* USER CODE BEGIN Includes */
+#include <stdbool.h>
+/* USER CODE END Includes */
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+void MX_USART1_UART_Init(void);
+
+/* USER CODE BEGIN Prototypes */
+/**
+ * @brief       Send raw data to usart1 port
+ * @param[in]   raw Raw data to send
+ * @param[in]   length Data length
+ */
+void uart1_hw_uart_send_raw(uint8_t* raw, uint32_t length);
+
+/**
+ * @brief       Control uart1 port
+ * @param[in]   enable TRUE - enable uart1 port
+                        FALSE - disable uart1 port
+ */
+void usart1_control(bool enable);
+
+/**
+ * @brief       New uart TX callback
+ * @param[in]   enable TRUE - Data is valid
+                        FALSE - Error orcurs
+ */
+void usart1_tx_complete_callback(bool status);
+
+
+/**
+ * @brief       New uart RX callback
+ * @param[in]   enable TRUE - Data is valid
+                        FALSE - Error orcurs
+ */
+void usart1_rx_complete_callback(bool status);
+
+
+/**
+ * @brief       Start DMA RX
+ */
+void usart1_start_dma_rx(void);
+
+/**
+ * @brief       Enable/Disable lpuart1 for rs485
+ * @param[in]   enable TRUE - Enable RS485
+                       DISABLE RS485
+ */
+void usart_lpusart_485_control(bool enable);
+
+/**
+ * @brief       Send data to rs485 port
+ * @param[in]   data RS485 tx data
+ * @param[in]   length RS485 data size
+ */
+void usart_lpusart_485_send(uint8_t *data, uint32_t length);
+/* USER CODE END Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __USART_H__ */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ 02.Firmware/DTG02_V3_Bootloader/Core/Src/main.c
@@ -0,0 +1,300 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "dma.h"
+#include "iwdg.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "app_eeprom.h"
+#include "measure_input.h"
+#include "lpf.h"
+#include "stdbool.h"
+#include "app_cli.h"
+#include "app_wdt.h"
+#include "app_sync.h"
+#include "hardware_manager.h"
+#include "app_bkup.h"
+#include "control_output.h"
+#include "gsm.h"
+#include "hardware.h"
+#include "modbus_master.h"
+#include "string.h" 
+#include "sys_ctx.h"
+#include "app_rtc.h"
+#include "app_debug.h"
+#include "app_spi_flash.h"
+#include "ota_update.h"
+#include "flash_if.h"
+#include "version_control.h"
+#include "utilities.h"
+#include "umm_malloc.h"
+#include "jig.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define WAKEUP_RESET_WDT_IN_LOW_POWER_MODE            28000    // ( ~16s)
+#define DEBUG_LOW_POWER                                 1
+#define DISABLE_GPIO_ENTER_LOW_POWER_MODE               0
+#define TEST_POWER_ALWAYS_TURN_OFF_GSM                  0
+#define TEST_OUTPUT_4_20MA                              0
+#define TEST_RS485                                      0
+#define TEST_INPUT_4_20_MA                              0
+#define TEST_BACKUP_REGISTER                            0
+#define TEST_DEVICE_NEVER_SLEEP							0
+#define TEST_CRC32										0
+#define CLI_ENABLE                                      1
+#define GSM_ENABLE										1
+typedef  void (*p_func)(void);
+p_func jump_to_application;
+uint32_t m_jump_addr;
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+/* memory for ummalloc */
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+	__enable_irq();
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_IWDG_Init();
+  MX_DMA_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+    ENABLE_INPUT_4_20MA_POWER(0);
+    RS485_POWER_EN(0);
+    LED1(0);
+
+    ota_flash_cfg_t cfg;
+	memcpy(&cfg, (ota_flash_cfg_t*)OTA_INFO_START_ADDR, sizeof(ota_flash_cfg_t));
+    if (cfg.flag != OTA_FLAG_UPDATE_NEW_FW)
+	{
+        
+        __disable_irq();
+		if (((*(__IO uint32_t*)APPLICATION_START_ADDR) & 0x2FFE0000) == 0x20000000)
+		{
+			/* Jump to user application */
+			m_jump_addr = *(__IO uint32_t*) (APPLICATION_START_ADDR + 4);
+			jump_to_application = (p_func) m_jump_addr;
+			/* Initialize user application's Stack Pointer */
+			__set_MSP(*(__IO uint32_t*) APPLICATION_START_ADDR);
+			jump_to_application();
+		}
+        while (1)
+        {
+            NVIC_SystemReset();
+        }
+	}
+	else
+	{
+        gsm_init_hw();
+	}
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+#if GSM_ENABLE
+	gsm_hw_layer_run();
+#endif
+      
+#if USE_SYNC_DRV
+	app_sync_polling_task();
+#else
+      static volatile uint32_t m_last_tick = 0;
+      if (sys_get_ms() - m_last_tick >= (uint32_t)1000)
+      {
+          m_last_tick = sys_get_ms();
+          gsm_mnr_task(NULL);
+      }
+#endif /* USE_SYNC_DRV */
+      
+#ifdef WDT_ENABLE
+    LL_IWDG_ReloadCounter(IWDG);
+#endif
+	
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLLMUL_3;
+  RCC_OscInitStruct.PLL.PLLDIV = RCC_PLLDIV_3;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
+  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK2;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+uint32_t sys_get_ms()
+{
+	return uwTick;
+}
+
+void sys_delay_ms(uint32_t ms)
+{
+	uint32_t current_tick = HAL_GetTick();
+	
+	while (1)
+	{
+#ifdef WDT_ENABLE
+        LL_IWDG_ReloadCounter(IWDG);
+#endif
+//		__WFI();	// hardfault when exit from stopmode
+		if (uwTick - current_tick >= (uint32_t)ms)
+		{
+			break;
+		}
+	}
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	NVIC_SystemReset();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
